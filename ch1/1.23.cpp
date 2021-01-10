c9db006e234e5e4a3767c785ab637563397fd034
attraverso <attraverso.dev@gmail.com>
2021-01-10 20:18:35 +0100

    applies own solution

--- ch1/1.23.cpp
+++ ch1/1.23.cpp
@@ -2,20 +2,23 @@
 #include "Sales_item.h"
 
 int main() {
-  Sales_item curItem, item;
-  if (std::cin >> curItem) {
-    int cnt = 1;
-    while (std::cin >> item) {
-      if (item.isbn() == curItem.isbn())
-        ++cnt;
-      else {
-        std::cout << curItem.isbn() << " " << cnt << std::endl;
-        curItem = item;
-        cnt = 1;
-      }
-    }
-    std::cout << curItem.isbn() << " " << cnt << std::endl;
-  }
 
-  return 0;
+	Sales_item refBook, currBook;
+	//get an input to establish a baseline. If there's one, the first thing is to set the count to one
+	if (std::cin >> refBook) {
+		int count = 1;
+		//then you start asking for other inputs and compare them
+		while (std::cin >> currBook) {
+			if (refBook.isbn() == currBook.isbn()) {
+				count++;
+			}
+			else {
+        //replace the whole item, who cares about the lost information in this case
+				refBook = currBook;
+        //reset the counter
+				count = 1;
+			}
+			std::cout << "isbn " << refBook.isbn() << " transactions " << count << std::endl;
+		}
+	}
 }
